fa7ab0a90c13bea217243b8bee30f57a819701b8
Preksha <preksha.shah17@gmail.com>
2020-10-11 14:10:24 -0600

    Update check_palindrome.py
    added some corner cases for comparison
    - remove special characters
    - ignore case
    - accept digits
    
    additional test cases
    return true/false

--- check_palindrome.py
+++ check_palindrome.py
@@ -1,19 +1,23 @@
+import re
+
+
 def check_palindrome(input_str: str) -> bool:
     """
     determine whether or not input string is palindrome
     :param input_str: string to evaluate
     :return: boolean value: true
     """
-    input_str = input_str.replace(" ", "")  # removing spaces
+    # removing spaces and special character, converting to lower case
+    input_str = re.sub('[\W_]+', '', input_str.replace(" ", "").lower())
     reverse_str = input_str[::-1]  # reverse string with slicing
-    if input_str == reverse_str:
-        return 1
-    return 0
+    return input_str == reverse_str
 
 
 if __name__ == "__main__":
     # test cases
-    assert check_palindrome("car") == 0
-    assert check_palindrome("test set") == 1
-    assert check_palindrome("race car") == 1
-    assert check_palindrome("what is this") == 0
+    assert not check_palindrome("car")
+    assert check_palindrome("test set")
+    assert check_palindrome("race car")
+    assert not check_palindrome("what is this")
+    assert check_palindrome("Go hang a salami - I'm a lasagna hog")
+    assert check_palindrome("test123321tset")
